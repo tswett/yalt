bed2c94bbad241f8789c25f6c297a0e8dff3c7bd
tannerswett <tannerswett@gmail.com>
2021-02-17 23:27:29 -0500

    testing: Refactoring

--- test/testing.lua
+++ test/testing.lua
@@ -1,29 +1,30 @@
-testing = require 'src/testing'
+local testing = require 'src/testing'
 
-suite = {}
+local suite = {}
 
-function suite.empty_test_suite_report()
-  local output = ''
+local function make_reporter()
+  local container = {output = ''}
   
   local function report(...)
     report_text = table.concat({...}, ' ')
-    output = output .. report_text .. ';'
+    container.output = container.output .. report_text .. ';'
   end
+  
+  return container, report
+end
+
+function suite.empty_test_suite_report()
+  container, report = make_reporter()
   
   local suite_to_test = {}
   
   testing.run_all_in_suite(suite_to_test, report)
   
-  return output == 'summary 0 ok;summary 0 failed;ok;'
+  return container.output == 'summary 0 ok;summary 0 failed;ok;'
 end
 
 function suite.failed_test_suite_reports_failed()
-  local output = ''
-  
-  local function report(...)
-    report_text = table.concat({...}, ' ')
-    output = output .. report_text .. ';'
-  end
+  container, report = make_reporter()
   
   local suite_to_test = {}
   
@@ -33,7 +34,7 @@ function suite.failed_test_suite_reports_failed()
   
   testing.run_all_in_suite(suite_to_test, report)
   
-  return output == 'summary 0 ok;summary 1 failed;failed;'
+  return container.output == 'summary 0 ok;summary 1 failed;failed;'
 end
 
 testing.run_all_in_suite(suite, print)
