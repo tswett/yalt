c91fa36544f0cc94d2c164d6b96a5bd687b2c61d
tannerswett <tannerswett@gmail.com>
2021-02-17 22:10:59 -0500

    testing: Report testing summary (number of successes, number of failures)

--- src/testing.lua
+++ src/testing.lua
@@ -1,17 +1,23 @@
 testing = {}
 
 function testing.run_all_in_suite(suite, report)
-  local success = true
+  local success_count = 0
+  local failure_count = 0
   
   for name, test_case in pairs(suite) do
     ran_ok, result = pcall(test_case)
     
-    if not ran_ok or (result ~= true) then
-      success = false
+    if ran_ok and (result == true) then
+      success_count = success_count + 1
+    else
+      failure_count = failure_count + 1
     end
   end
   
-  if success then
+  report('summary', success_count, 'ok')
+  report('summary', failure_count, 'failed')
+  
+  if failure_count == 0 then
     report('ok')
   else
     report('failed')
--- test/testing.lua
+++ test/testing.lua
@@ -2,6 +2,21 @@ testing = require 'src/testing'
 
 suite = {}
 
+function suite.empty_test_suite_report()
+  local output = ''
+  
+  function report(...)
+    report_text = table.concat({...}, ' ')
+    output = output .. report_text .. ';'
+  end
+  
+  local suite_to_test = {}
+  
+  testing.run_all_in_suite(suite_to_test, report)
+  
+  return output == 'summary 0 ok;summary 0 failed;ok;'
+end
+
 function suite.failed_test_suite_reports_failed()
   local suite_to_test = {}
   function suite_to_test.fail()
