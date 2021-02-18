dfe5e70ef708ac852ac6b5639995842bc79891db
tannerswett <tannerswett@gmail.com>
2021-02-17 22:28:18 -0500

    testing: Update implementation of failed_test_suite_reports_failed

--- test/testing.lua
+++ test/testing.lua
@@ -5,7 +5,7 @@ suite = {}
 function suite.empty_test_suite_report()
   local output = ''
   
-  function report(...)
+  local function report(...)
     report_text = table.concat({...}, ' ')
     output = output .. report_text .. ';'
   end
@@ -18,19 +18,22 @@ function suite.empty_test_suite_report()
 end
 
 function suite.failed_test_suite_reports_failed()
+  local output = ''
+  
+  local function report(...)
+    report_text = table.concat({...}, ' ')
+    output = output .. report_text .. ';'
+  end
+  
   local suite_to_test = {}
+  
   function suite_to_test.fail()
     return false
   end
   
-  local result
-  local function report_result(r)
-    result = r
-  end
+  testing.run_all_in_suite(suite_to_test, report)
   
-  testing.run_all_in_suite(suite_to_test, report_result)
-  
-  return result == 'failed'
+  return output == 'summary 0 ok;summary 1 failed;failed;'
 end
 
 testing.run_all_in_suite(suite, print)
